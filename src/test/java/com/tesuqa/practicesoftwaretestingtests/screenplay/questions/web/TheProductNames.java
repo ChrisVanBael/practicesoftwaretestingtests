d0eb820a6a435f89731e4d00938ae66e54353834
Chris Van Bael <chris.van.bael@gmail.com>
2023-11-12 19:40:08 +0100

    Fix homepage locator

--- src/test/java/com/tesuqa/practicesoftwaretestingtests/screenplay/questions/web/TheProductNames.java
+++ src/test/java/com/tesuqa/practicesoftwaretestingtests/screenplay/questions/web/TheProductNames.java
@@ -1,5 +1,6 @@
 package com.tesuqa.practicesoftwaretestingtests.screenplay.questions.web;
 
+import com.tesuqa.practicesoftwaretestingtests.pages.HomePage;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.questions.TextContent;
@@ -10,16 +11,14 @@ import java.util.stream.Collectors;
 
 public class TheProductNames {
 
-    static By PRODUCT_NAMES = By.xpath("//h5[@data-test='product-name']");
-
     /**
      * Returns a list of product names on the homepage
      * @return list of product names
      */
     public static Question<List<String>> onTheHomePage() {
-        return Question.about("the productnames listed on the home page")
+        return Question.about("the product names listed on the home page")
                 .answeredBy(
-                        actor -> BrowseTheWeb.as(actor).findAll(PRODUCT_NAMES).textContents()
+                        actor -> BrowseTheWeb.as(actor).findAll(HomePage.PRODUCT_NAMES).textContents()
                                 .stream()
                                 .map(entry -> entry.trim())
                                 .collect(Collectors.toList())
